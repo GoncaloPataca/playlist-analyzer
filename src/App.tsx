d7b8864ca69cc174304c55e1657fd106f067126b
Gonçalo Pataca <gonzalo.nascimento.pataca@gmail.com>
2025-05-29 17:53:09 +0100

    Revert "install ag-grid and setup website wireframe structure"
    This reverts commit 04595ba835d20b352c4165a42e212bbaaf605ef1.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-import { SidePanel } from "./components/SidePanel";
-import { TrackGrid } from "./components/TrackGrid";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import { PlaylistDetails } from "./components/PlaylistDetails";
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="flex h-screen w-screen">
-      <SidePanel />
-      <main className="flex-1 p-6 overflow-auto">
-        <h1 className="text-2xl font-semibold mb-6">Main Page</h1>
-        <PlaylistDetails />
-        <div className="overflow-x-auto">
-          <TrackGrid />
-        </div>
-      </main>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
