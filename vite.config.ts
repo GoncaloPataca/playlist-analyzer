201ce9f4f98953c00e75cfd6849248aa1216b7ec
Gonçalo Pataca <gonzalo.nascimento.pataca@gmail.com>
2025-06-09 20:41:12 +0100

    feat: add vite-tsconfig-paths for improved module resolution and update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import mkcert from "vite-plugin-mkcert";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), mkcert()],
+  plugins: [react(), tailwindcss(), mkcert(), tsconfigPaths()],
   server: {
     host: true,
   },
